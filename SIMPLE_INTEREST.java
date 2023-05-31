3ed3829c9c225744c273d7f10c1f6f12cffc8728
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-05-31 20:59:02 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         int p,t,r,si;         Scanner sc=new Scanner(System.in);         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         si=(p*t*r)/100;         System.out.println(si);     } }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        int p,t,r,si;
+        Scanner sc=new Scanner(System.in);
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        si=(p*t*r)/100;
+        System.out.println(si);
+    }
+}
