e7c970992da88e17076c2812b736fd73de548df4
22P31A0418 <119720034+22P31A0418@users.noreply.github.com>
2023-08-05 19:22:16 +0530

    import java.util.*; public class Sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String userInput=sc.nextLine();         System.out.println(""+userInput);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String userInput=sc.nextLine();
+        System.out.println(""+userInput);
+        sc.close();
+    }
+}
